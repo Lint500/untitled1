9aabe541675c9df7e41eac42ce1c7e4346732810
abc <1019097627@qq.com>
2026-03-27 10:47:04 +0800

    inital commit

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,10 +1,6 @@
 // 图片资源
 export { default as logo192 } from './images/logo192.png';
 
-// 后续添加其他资源...
-// export { default as otherImage } from './images/other.png';
-
-
 
 
 
--- src/types/electron.d.ts
+++ src/types/electron.d.ts
@@ -1,4 +1,36 @@
 /* Electron API 类型声明 */
+
+/* 静态资源模块声明 */
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.gif' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.svg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.ico' {
+  const value: string;
+  export default value;
+}
+
 export interface ElectronAPI {
   auth: {
     check: () => Promise<{ role: string; isDeveloper: boolean }>;
